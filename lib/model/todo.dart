0ad4aa171bb26e1bb9586f2c2b98a4e375514b2b
@manuelduarte077 <carlosd99m@gmail.com>
2022-07-27 10:22:45 -0600

    chore(core): add model

---
+++ lib/model/todo.dart
@@ -0,0 +1,31 @@
+class Todo {
+  String _title;
+  String _description;
+  bool _isFinished;
+
+  Todo({
+    required String title,
+    required String description,
+    required bool isFinished,
+  })  : _title = title,
+        _description = description,
+        _isFinished = isFinished;
+
+  get title => _title;
+
+  set title(value) {
+    _title = value;
+  }
+
+  get description => _description;
+
+  set description(value) {
+    _description = value;
+  }
+
+  get isFinished => _isFinished;
+
+  set isFinished(value) {
+    _isFinished = value;
+  }
+}
